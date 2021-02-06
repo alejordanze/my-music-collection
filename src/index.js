2846cd2109bf40579b51d12c1b3c6563e5469fb5
alejordanze <alejordanze@gmail.com>
2021-02-06 18:51:56 -0400

    adding swagger documentation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('../swagger.json');
 const albumRoutes = require('./routes/albumRoutes');
 const artistRoutes = require('./routes/artistRoutes');
 const albumArtistRoutes = require('./routes/albumArtistRoutes');
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/albums', albumRoutes);
 app.use('/api/artists', artistRoutes);
 app.use('/api/albumArtist',albumArtistRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
